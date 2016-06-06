251a69e3d582b4ea5f655a28710e66f10706ba16
SajithaSahadevan <gssajithaa@gmail.com>
2016-06-06 15:50:39 +0530

    Update char_or_not.c

--- char_or_not.c
+++ char_or_not.c
@@ -4,8 +4,8 @@ int main(void) {
 	char a;
 	scanf("%c",&a);
 	if(((a>64)&&(a<=90))||((a>96)&&(a<=122)))
-	printf("CHARACTER");
+	printf("ALPHABET");
 	else
-	printf("NOT A CHARACTER");
+	printf("NOT AN ALPHABET");
 	return 0;
 }
